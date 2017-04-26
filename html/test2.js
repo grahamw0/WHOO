7837c2b04f1c33a00c7ba8f0e9c41db77e5a5eb0
Greg Fletcher <gnfletcher@gmail.com>
2017-04-26 10:06:23 -0400

    Changes.

--- html/test2.js
+++ html/test2.js
@@ -1,60 +1,61 @@
-function process(){
-    var accessToken = "c87528439d4f4a2aa097f1aec7d414e3";
-    var baseUrl = "https://api.api.ai/v1/";
-    $(document).ready(function() {
-	$("#input").keypress(function(event) {
-	    if (event.which == 13 && $("#input").val().replace(/\s/g, "") != "") {
-		event.preventDefault();
-		send();
-	    }
-	});
-	$("#rec").click(function(event) {
-	    switchRecognition();
-	});
+var count = 0;
+var accessToken = "c87528439d4f4a2aa097f1aec7d414e3";
+var baseUrl = "https://api.api.ai/v1/";
+$(document).ready(function() {
+    $("#input").keypress(function(event) {
+    	if (event.which == 13 && $("#input").val().replace(/\s/g, "") != "") {  // Ensuring input not blank
+    	    event.preventDefault();
+    	    send();
+    	}
     });
-    var recognition;
-    function startRecognition() {
-	recognition = new webkitSpeechRecognition();
-	recognition.onstart = function(event) {
-	    updateRec();
-	};
-	recognition.onresult = function(event) {
-	    var text = "";
-	    for (var i = event.resultIndex; i < event.results.length; ++i) {
-		text += event.results[i][0].transcript;
-	    }
-	    setInput(text);
-	    stopRecognition();
-	};
-	recognition.onend = function() {
-	    stopRecognition();
-	};
-	recognition.lang = "en-US";
-	recognition.start();
+    $("#rec").click(function(event) {
+    	switchRecognition();
+    });
+    $("#input").focus();
+});
+var recognition;
+function startRecognition() {
+    recognition = new webkitSpeechRecognition();
+    recognition.onstart = function(event) {
+    	updateRec();
+    };
+    recognition.onresult = function(event) {
+    	var text = "";
+    	for (var i = event.resultIndex; i < event.results.length; ++i) {
+    	    text += event.results[i][0].transcript;
+    	}
+    	setInput(text);
+    	stopRecognition();
+    };
+    recognition.onend = function() {
+    	stopRecognition();
+    };
+    recognition.lang = "en-US";
+    recognition.start();
+}
+
+function stopRecognition() {
+    if (recognition) {
+    	recognition.stop();
+    	recognition = null;
     }
-    
-    function stopRecognition() {
-	if (recognition) {
-	    recognition.stop();
-	    recognition = null;
-	}
-	updateRec();
+    updateRec();
+}
+function switchRecognition() {
+    if (recognition) {
+    	stopRecognition();
+    } else {
+    	startRecognition();
     }
-    function switchRecognition() {
-	if (recognition) {
-	    stopRecognition();
-	} else {
-	    startRecognition();
-	}
-    }
-    function setInput(text) {
-	$("#input").val(text);
-	send();
-    }
-    function updateRec() {
-	$("#rec").text(recognition ? "Stop" : "Speak");
-    }
-    function send() {
+}
+function setInput(text) {
+    $("#input").val(text);
+    send();
+}
+function updateRec() {
+    $("#rec").text(recognition ? "Stop" : "Speak");
+}
+function send() {
     var text = $("#input").val();
     setResponse(text, true);  // User input
     $.ajax({
@@ -73,8 +74,10 @@ function process(){
     	    setResponse("Internal Server Error");
     	}
     });
-	// setResponse("Loading...");
-    }
+    //setResponse("Loading...");
+}
+
+// val is text to be displayed, userInput is true if user input, false if Bot response
 function setResponse(val, userInput) {
     $("#input").val("");  // Blank out user input box
 
@@ -91,5 +94,6 @@ function setResponse(val, userInput) {
 	}
     }
     $("#divCont").scrollTop($("#divCont").prop("scrollHeight"));
-   
+
+    
 }
