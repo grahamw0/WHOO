8cdf4a34ae52b066bf4f10b235374d2038d30141
Greg Fletcher <gnfletcher@gmail.com>
2017-04-26 10:22:38 -0400

    Changes.

--- html/test2.js
+++ html/test2.js
@@ -1,57 +1,59 @@
 var count = 0;
- var accessToken = "c87528439d4f4a2aa097f1aec7d414e3";
-    var baseUrl = "https://api.api.ai/v1/";
-    $(document).ready(function() {
-	$("#input").keypress(function(event) {
-	    if (event.which == 13 && $("#input").val().replace(/\s/g, "") != "") {
-		event.preventDefault();
-		send();
-	    }
-	});
-	$("#rec").click(function(event) {
-	    switchRecognition();
-	});
-    var recognition;
-    function startRecognition() {
-	recognition = new webkitSpeechRecognition();
-	recognition.onstart = function(event) {
-	    updateRec();
-	};
-	recognition.onresult = function(event) {
-	    var text = "";
-	    for (var i = event.resultIndex; i < event.results.length; ++i) {
-		text += event.results[i][0].transcript;
-	    }
-	    setInput(text);
-	    stopRecognition();
-	};
-	recognition.onend = function() {
-	    stopRecognition();
-	};
-	recognition.lang = "en-US";
-	recognition.start();
+var accessToken = "c87528439d4f4a2aa097f1aec7d414e3";
+var baseUrl = "https://api.api.ai/v1/";
+$(document).ready(function() {
+    $("#input").keypress(function(event) {
+    	if (event.which == 13 && $("#input").val().replace(/\s/g, "") != "") {  // Ensuring input not blank
+    	    event.preventDefault();
+    	    send();
+    	}
+    });
+    $("#rec").click(function(event) {
+    	switchRecognition();
+    });
+    $("#input").focus();
+});
+var recognition;
+function startRecognition() {
+    recognition = new webkitSpeechRecognition();
+    recognition.onstart = function(event) {
+    	updateRec();
+    };
+    recognition.onresult = function(event) {
+    	var text = "";
+    	for (var i = event.resultIndex; i < event.results.length; ++i) {
+    	    text += event.results[i][0].transcript;
+    	}
+    	setInput(text);
+    	stopRecognition();
+    };
+    recognition.onend = function() {
+    	stopRecognition();
+    };
+    recognition.lang = "en-US";
+    recognition.start();
+}
+
+function stopRecognition() {
+    if (recognition) {
+    	recognition.stop();
+    	recognition = null;
     }
-    
-    function stopRecognition() {
-	if (recognition) {
-	    recognition.stop();
-	    recognition = null;
-	}
-	updateRec();
+    updateRec();
+}
+function switchRecognition() {
+    if (recognition) {
+    	stopRecognition();
+    } else {
+    	startRecognition();
     }
-    function switchRecognition() {
-	if (recognition) {
-	    stopRecognition();
-	} else {
-	    startRecognition();
-	}
-    }
-    function setInput(text) {
-	$("#input").val(text);
-	send();
-    }
+}
+function setInput(text) {
+    $("#input").val(text);
+    send();
+}
 function updateRec() {
-    $("#rec").text(recognition ? "Stop" : "Speak");
+    $("#rec").text(recognition ? "Stop" : "<i class="fa fa-microphone">");
 }
 function send() {
     var text = $("#input").val();
